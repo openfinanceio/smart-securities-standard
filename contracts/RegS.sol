985cb8b5b7e75d210ea6de2e9b52653c883cf97c
tmcinerney <tmack.iv@gmail.com>
2018-03-15 15:05:01 -0500

    REGSTOKEN.SOL

--- contracts/RegS.sol
+++ contracts/RegS.sol
@@ -4,6 +4,7 @@ import './TransferRestrictor.sol';
 
 /// @title An implementation of restrictions under regulation S Category 3
 contract RegS is TransferRestrictor {
+  function startTrading() public;
   function registerAmlKycChecker(address _checker, address _token) public;
   function registerAccreditationChecker(address _checker, address _token) public;
 }
