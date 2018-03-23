55a2389e30b760893621b96ad42ca1c836b85768
Ian <ian.shipman@cfxtrading.com>
2018-03-23 16:22:25 -0500

    updates interface

--- contracts/CapTables.sol
+++ contracts/CapTables.sol
@@ -1,33 +1,38 @@
 pragma solidity ^0.4.18;
 
 import "./IndexConsumer.sol";
+import "./zeppelin-solidity/contracts/math/SafeMath.sol";
 
 /**
  * The sole purpose of this contract is to store the cap tables of securities
  * created by the OFN system.  We take the position that a security is defined
  * by its cap table and not by its transfer rules.  So a security is
- * represented by a unique integer index.
+ * represented by a unique integer index.  A security has a fixed amount and we
+ * preserve this invariant by allowing no other cap table updates beside
+ * transfers.
  */
 contract CapTables is IndexConsumer {
-  /** `capTable(security, user) == userBalance` */
-  mapping(uint256 => mapping(address => uint256)) public capTable;
+  using SafeMath for uint256;
   /** Address of security */
   mapping(uint256 => address) public addresses;
-  /** Set the  balance. */
-  function set(uint256 security, address user, uint256 balance) public {
-    require(msg.sender == addresses[security]);
-    capTable[security][user] = balance;
-  }
+  /** `capTable(security, user) == userBalance` */
+  mapping(uint256 => mapping(address => uint256)) public capTable;
   /** Add a security to the contract. */
-  function initialize(address security, uint256 supply) public returns (uint256) {
-    require(msg.sender == security);
+  function initialize(uint256 supply) public returns (uint256) {
     uint256 index = nextIndex();
-    addresses[index] = security;
-    capTable[index][security] = supply;
+    addresses[index] = msg.sender;
+    capTable[index][msg.sender] = supply;
   }
   /** Migrate a security to a new address, if its transfer restriction rules change. */
   function migrate(uint256 index, address newAddress) public {
     require(msg.sender == addresses[index]);
     addresses[index] = newAddress;
+  }
+  /** Transfer an amount of security. */
+  function transfer(uint256 security, address src, address dest, uint256 amount) public {
+    require(msg.sender == addresses[security]);
+    require(capTable[security][src] >= amount);
+    capTable[security][src].sub(amount);
+    capTable[security][dest].add(amount);
   }
 }
