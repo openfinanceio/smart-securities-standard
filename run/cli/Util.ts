ffc6a094111a6dbf6174b1900c892a683d0f3079
Kael Shipman <kael.shipman@cfxtrading.com>
2020-03-30 13:15:25 -0500

    Updated deps, including bignumber

--- run/cli/Util.ts
+++ run/cli/Util.ts
@@ -2,5 +2,5 @@ import { BigNumber } from "bignumber.js";
 import { addHexPrefix } from "ethereumjs-util";
 
 export function gweiToWei(this: void, gwei: number) {
-  return addHexPrefix(new BigNumber(gwei).mul(1e9).toString(16));
+  return addHexPrefix(new BigNumber(gwei).times(1e9).toString(16));
 }
